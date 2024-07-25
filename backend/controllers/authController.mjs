8f84fb6869b740beb6d1b53c54ee9c801ac70be8
Jenny <jennylee1989@gmail.com>
2024-07-24 21:42:42 -0700

    debugging

--- backend/controllers/authController.mjs
+++ backend/controllers/authController.mjs
@@ -15,43 +15,51 @@ const handleLogin = async (req, res) => {
     const foundUser = await User.findOne({ username: user }).exec();
     if (!foundUser) return res.sendStatus(401); //Unauthorized
   } catch (err) {
-    console.error(err);
+    console.error("error in found User");
     return res.sendStatus(500);
   }
   // evaluate password
-  const match = await bcrypt.compare(pwd, foundUser.password);
+  try {
+    const match = await bcrypt.compare(pwd, foundUser.password);
+  } catch (err) {
+    console.log("error in match bcrypt");
+    return res.sendStatus(500);
+  }
   if (match) {
-    const roles = Object.values(foundUser.roles).filter(Boolean);
-    // create JWTs
-    const accessToken = jwt.sign(
-      {
-        UserInfo: {
-          username: foundUser.username,
-          roles: roles,
+    try {
+      const roles = Object.values(foundUser.roles).filter(Boolean);
+      // create JWTs
+      const accessToken = jwt.sign(
+        {
+          UserInfo: {
+            username: foundUser.username,
+            roles: roles,
+          },
         },
-      },
-      process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "10s" }
-    );
-    const refreshToken = jwt.sign(
-      { username: foundUser.username },
-      process.env.REFRESH_TOKEN_SECRET,
-      { expiresIn: "1d" }
-    );
-    // Saving refreshToken with current user
-    foundUser.refreshToken = refreshToken;
-    const result = await foundUser.save();
-    console.log(result);
-    console.log(roles);
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "10s" }
+      );
+      const refreshToken = jwt.sign(
+        { username: foundUser.username },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: "1d" }
+      );
+      // Saving refreshToken with current user
+      foundUser.refreshToken = refreshToken;
+      const result = await foundUser.save();
+      console.log(result);
+      console.log(roles);
+    } catch (err) {
+      console.error("suuuuper error");
+    }
 
     // Creates Secure Cookie with refresh token
-    // res.cookie("jwt", refreshToken, {
-    //   httpOnly: true,
-    //   secure: true,
-    //   sameSite: "None",
-    //   domain: "alternateshotmarketing.com",
-    //   maxAge: 24 * 60 * 60 * 1000,
-    // });
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
 
     // Send authorization roles and access token to user
     res.json({ roles, accessToken, isAuthenticated: true });
