29a106c4c9a823ada1f202dcd3a82e44b53283db
Jenny <jennylee1989@gmail.com>
2024-07-24 20:44:13 -0700

    logging

--- backend/server/server.mjs
+++ backend/server/server.mjs
@@ -23,6 +23,11 @@ app.use(
     credentials: true,
   })
 );
+
+app.use((req, res, next) => {
+  console.log("Request received:", req.method, req.url);
+  next();
+});
 // app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
